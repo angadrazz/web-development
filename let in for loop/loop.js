97c9c1806e4a1923b237228aca7785c308b24576
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-24 17:39:25 +0530

    functionm

--- let in for loop/loop.js
+++ let in for loop/loop.js
@@ -1,8 +1,86 @@
-var arr = new Array(6);
-for (let i = 1; i <= 5; i++){
-        i++;
-        arr[i] = function(){
-                console.log(i);
-        }
-        i--;
-}
+// var arr = new Array(6);
+// for (let i = 1; i <= 5; i++){
+//         i++;
+//         arr[i] = function(){
+//                 console.log(i);
+//         }
+//         i--;
+// }
+// for(var a = 1; a < 5; a++){
+//         setTimeout(function(){
+//         console.log(a)}, 1000);
+//         } 
+
+//         for(let a = 1; a < 5; a++){
+//                 setTimeout(function(){
+//                 console.log(a)}, 1000);
+//             } 
+
+
+            console.log((function(x, f = () => x) {
+                var x;
+                var y = x;
+                x = 2;
+                return [x, y, f()];
+              })(1));
+
+              let user1 = {
+                firstname: 'John',
+                lastname: 'Doe',
+                getFullName: function(){
+                    return()=>{
+                        console.log(`The full name of the user is ${this.firstname} ${this.lastname} `);
+                    }
+                }    
+             }
+             user1.getFullName()();
+
+             console.log('New Question');
+
+             let user = {
+                firstname: 'John',
+                lastname: 'Doe',
+                getFullName: function(){
+                    return() => {
+                        console.log(`The full name of the user is ${this.firstname} ${this.lastname} `);
+                    }
+                }    
+             }
+             user.getFullName()();
+
+
+             function message(params) 
+                {
+                        let message = "Hello";
+                        console.log(message); 
+                      }
+                      console.log(message);
+
+                      function makeCounter() {
+                        let count = 0;
+                        return function() {
+                          return count++;
+                        };
+                      }
+                      let counter1 = makeCounter();
+                      let counter2 = makeCounter();
+                      console.log( counter1() ); 
+                      console.log( counter1() ); 
+                      console.log( counter2() );
+                      
+                      function makeArmy() {
+                        let shooters = [];
+                        let i = 0;
+                        while (i < 10) {
+                          let shooter = function() {
+                            console.log( i ); 
+                          };
+                          shooters.push(shooter);
+                ``          i++;
+                        }
+                        return shooters;
+                      }
+                      let army = makeArmy();
+                      army[0]();
+                      army[5]();
+                      
