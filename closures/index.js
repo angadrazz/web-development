61797b27f5866a31a6e0028ade8861012ff28f10
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-24 12:37:45 +0530

    inner

--- closures/index.js
+++ closures/index.js
@@ -3,13 +3,14 @@ var i = 10;
 function outer () {
         var j = 20;
         console.log(i,j);
-        function inner () {
+        var inner  = function() {
                 var k = 30;
                 console.log(j,j,k);
 
         }
-        inner();
+        return inner();
 }
 
-outer();
+var inner = outer();
+inner();
 
