7b80aa22a49e29bb61ad7d720650e53333e5eb4b
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-28 14:32:14 +0530

    i create a funtion var

--- function to create object/object.js
+++ function to create object/object.js
@@ -17,3 +17,12 @@ function createStudent(name, rollNo, marks){
 
 var student1 = createStudent("angad", 58, 32);
 var student2 = createStudent("manish", 54, 36);
+var student3 = createStudent("manish", 54, 36);
+var student4 = createStudent("manish", 54, 36);
+var student5 = createStudent("manish", 54, 36);
+var student6 = createStudent("manish", 54, 36);
+var student7 = createStudent("manish", 54, 36);
+var student8 = createStudent("manish", 54, 36);
+
+console.log(student1,student3,student4,student5,student6,student7,student8);
+console.log(student2);
