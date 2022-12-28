53c64928ffff35965b7f317e3b1f6b737b29a159
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-28 11:50:09 +0530

    var joe = {     name: "Joe",     hello: function (){        console.log("hi, I am " + this.name) ;     } }
    var globalHello1 = joe.hello.bind(joe) ;
    var globalHello2 = joe.hello ;
    globalHello1();
    globalHello2.bind(joe)();

--- iife/iife.js
+++ iife/iife.js
@@ -51,6 +51,18 @@ var joe = {
  }
 
  print.apply(joe);
- //print(joe);
+ print(joe);
+
  print.bind(joe)() ;
- print.bind(joe)() ;
+
+ var joe = {
+    name: "Joe",
+    hello: function (){
+       console.log("hi, I am " + this.name) ;
+    }
+}
+
+var globalHello1 = joe.hello.bind(joe) ;
+var globalHello2 = joe.hello ;
+globalHello1();
+globalHello2.bind(joe)();
