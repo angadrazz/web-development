f61b1530ad687153cff8c55c75ef7b373d863d94
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-28 16:52:12 +0530

    g

--- function to create object/object.js
+++ function to create object/object.js
@@ -7,6 +7,9 @@
 // }
 // console.log(student);
 
+
+//What is JavaScript
+
 function createStudent(name, rollNo, marks){
         var student = {};
         student.name = name;
@@ -26,3 +29,20 @@ var student8 = createStudent("manish", 54, 36);
 
 console.log(student1,student3,student4,student5,student6,student7,student8);
 console.log(student2);
+
+
+
+What is JavaScript :-
+JavaScript is a programming language initially designed to interact with
+ elements of web pages. 
+
+In web browsers, JavaScript consists of three main parts:
+
+1.ECMAScript provides the core functionality.
+2.The Document Object Model (DOM) provides interfaces for interacting
+ with elements on web pages
+3.The Browser Object Model (BOM) provides the browser API for interacting with the web browser.
+Example:- 
+         var x = 10;
+         var y = 20;
+     console.log(x + y); //30
