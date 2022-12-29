4ba7e1ad448f53825d82524722163f65a1bb16c8
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 14:19:02 +0530

    code

---
+++ object/objprototype.js
@@ -0,0 +1,13 @@
+function vehicle (numwheels, price){
+        this.numwheels = numwheels;
+        this.price = price;
+}
+
+vehicle.prototype.getPrice = function(){
+        return this.price;
+
+
+}
+
+var vehicle1 = new vehicle(4, 54000);
+var vehicle2 = new vehicle(6, 65400);
