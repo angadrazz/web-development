f6cea4f301c96f2bdf5ecc04c5b81e56d9972f54
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 19:35:37 +0530

    que3

--- Assignment/task.js
+++ Assignment/task.js
@@ -5,9 +5,18 @@
 //        bike();
 
 
-"use strict"
-function bike() {
-    console.log(this.name);
-}
-var name = "Ninja";
-bike();
+// "use strict"
+// function bike() {
+//     console.log(this.name);
+// }
+// var name = "Ninja";
+// bike();
+
+
+function Abc() {
+        "use strict"
+        this.a = 25;
+        return {a: 35, b: 44};
+        }
+        var obj = new Abc();
+        console.log(obj.a, obj.b);
