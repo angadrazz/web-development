48b671e553fe8cae3e4a34d3708362e436a7f3d8
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 18:52:25 +0530

    solved

--- class/classprototype.js
+++ class/classprototype.js
@@ -1,12 +1,14 @@
-class vehicle () {
+class vehicle {
         constructor(numWheels, price){
                 this.numWheels = numWheels;
                 this.price = price;
         }
-        getPrice(){
+        getprice() {
                 return this.price;
         }
-}
+                
+        }
+
 
 var s1 = new vehicle(2, 54000);
 var s2 = new vehicle(4,87000);
