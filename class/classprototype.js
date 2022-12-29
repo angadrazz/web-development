b94eecd0a0b6a89e955f9a6b21e186f8057144f9
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 18:58:37 +0530

    class Person {    constructor(name) { this.name = name; } } console.log(typeof( Person));

--- class/classprototype.js
+++ class/classprototype.js
@@ -7,11 +7,17 @@ class vehicle {
                 return this.price;
         }
                 
-        }
-
+        
+}
 
 var s1 = new vehicle(2, 54000);
 var s2 = new vehicle(4,87000);
 
 console.log(s1);
 console.log(s2);
+
+
+class Person {
+        constructor(name) { this.name = name; }
+     }
+     console.log(typeof( Person));
