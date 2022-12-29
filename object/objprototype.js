bb719985fb7033470be5f184afe0f4220a47a694
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 14:28:40 +0530

    function A(){     this.name = "A";     this.color = "blue"; } function B(){     this.name = "B"; } function C(){     this.name =  "C"; }
    let obj1 = new A() ;
    let obj2 = new B() ;
    let obj3 = new Object();
    let obj4 = new C();
    
    A.prototype.color = "red" ;
    B.prototype.color = "red" ;
    Object.prototype.color = "pink" ;
    
    console.log(obj1.color, obj2.color, obj4.color, obj3.color);

--- object/objprototype.js
+++ object/objprototype.js
@@ -24,4 +24,27 @@ function A(){
    var d = new A();
    
    console.log(d.color);
-   console.log(A.color);
+   console.log("question enxt");
+
+   function A(){
+        this.name = "A";
+        this.color = "blue";
+    }
+    function B(){
+        this.name = "B";
+    }
+    function C(){
+        this.name =  "C";
+    }
+    
+    let obj1 = new A() ;
+    let obj2 = new B() ;
+    let obj3 = new Object();
+    let obj4 = new C();
+    
+    A.prototype.color = "red" ;
+    B.prototype.color = "red" ;
+    Object.prototype.color = "pink" ;
+    
+    console.log(obj1.color, obj2.color, obj4.color, obj3.color);
+
