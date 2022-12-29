f2820958b3a25823954537dab1d93445ae899b26
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2022-12-29 14:22:01 +0530

    function A(){      this.name =  "A"; }
    Object.prototype.color = "red";
    var d = new A();
    
    console.log(d.color) ;

--- object/objprototype.js
+++ object/objprototype.js
@@ -15,3 +15,12 @@ var vehicle2 = new vehicle(6, 65400);
 
 console.log(vehicle1);
 console.log(vehicle2);
+
+function A(){
+        this.name =  "A";
+   }
+   
+   Object.prototype.color = "red";
+   var d = new A();
+   
+   console.log(d.color) ;
